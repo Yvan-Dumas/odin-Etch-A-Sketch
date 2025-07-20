01e668bd4b42e1eef07cce82c7e1b6625e603296
Yvan <yvandumas05@gmail.com>
2025-07-20 16:54:42 +0200

    Added functions to create the grid and to change the size of the grid between 1 and 100

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
+const container = document.querySelector(".container");
+const buttonSizeGrid = document.querySelector("#gridSize");
+const buttonReset = document.querySelector("#reset")
+
 function changeColor(element) {
     element.style.backgroundColor = "red";
 }
 
-const container = document.querySelector(".container");
+let currentSize;
 
-for (let i = 0; i<256; i++) {
-    const div = document.createElement("div");
-    div.classList.add("squareDivs");
-    container.appendChild(div);
-    div.addEventListener("mouseover", () => changeColor(div))
+function createGrid(size) {
+    container.innerHTML ="";
+
+    const squareSize = 960/size;
+
+    for (let i = 0; i < size * size; i++) {
+        const div = document.createElement("div");
+        div.classList.add("squareDivs");
+        div.style.height = squareSize+"px";
+        div.style.width = squareSize+"px";
+        container.appendChild(div);
+        div.addEventListener("mouseover", () => changeColor(div))
+    }
 }
+
+function changeSize() {
+    let size;
+    do {
+        size = prompt("Wich size do you want ? (max 100)")
+    } while ((size > 100) || (size < 1));
+
+    createGrid(size);
+    currentSize = size;
+}
+
+buttonSizeGrid.addEventListener("click", changeSize);
+buttonReset.addEventListener("click", () => createGrid(currentSize))
